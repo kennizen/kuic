1b95151834a34d5073b576abc39d164860dadf7b
kennizen <prachurjyagogoi123@gmail.com>
2024-04-18 00:06:11 +0530

    added files component

--- src/app/application/FilePicker.tsx
+++ src/app/application/FilePicker.tsx
@@ -1,7 +1,53 @@
+"use client";
 
+import { ChangeEvent, ReactNode, useRef, useState } from "react";
 
-const FilePicker = () => {
-   return <div>FilePicker</div>;
+interface IProps {
+  children: ReactNode;
+  onFiles: (files: File[]) => void;
+}
+
+const FilePicker = ({ children, onFiles }: IProps) => {
+  // hooks
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  // methods
+  function handleAddFiles() {
+    if (!inputRef || !inputRef.current) return;
+
+    inputRef.current.click();
+  }
+
+  function handleOnChange(e: ChangeEvent<HTMLInputElement>) {
+    const fileList = e.target.files;
+
+    if (fileList === null) {
+      onFiles([]);
+      return;
+    }
+
+    const fi: File[] = [];
+
+    for (let i = 0; i < fileList.length; ++i) {
+      const file = fileList.item(i);
+      if (file) fi.push(file);
+    }
+
+    onFiles(fi);
+  }
+
+  return (
+    <div className="w-[22rem] border border-slate-600 h-[30rem] rounded-md overflow-auto p-4 flex flex-col items-end gap-4">
+      <button
+        onClick={handleAddFiles}
+        className="px-2 py-1 rounded-md bg-blue-600 text-sm hover:bg-blue-700 transition-colors"
+      >
+        Add Files
+      </button>
+      <input type="file" hidden ref={inputRef} multiple onChange={handleOnChange} />
+      {children}
+    </div>
+  );
 };
 
 export default FilePicker;
--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import ConnectionStatus from "./ConnectionStatus";
 import UserType from "./UserType";
 import WillNotWork from "./WillNotWork";
+import FilePicker from "./FilePicker";
 
 const Application = () => {
   // states
@@ -19,9 +20,14 @@ const Application = () => {
 
   return (
     <div className="h-[calc(100vh-var(--navbar-height))] text-slate-100 flex justify-center">
-      <div className="min-w-[400px] w-[50%] py-8 flex justify-between flex-wrap gap-4">
+      <div className="max-w-[60rem] w-full py-8 flex justify-between flex-wrap gap-4 p-4">
         <UserType />
-        <ConnectionStatus type="checking" />
+        <div className="flex flex-col gap-10 items-end">
+          <ConnectionStatus type="checking" />
+          <FilePicker onFiles={(files) => console.log(files)}>
+            <div>xcd</div>
+          </FilePicker>
+        </div>
       </div>
     </div>
   );
