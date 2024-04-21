564aaaed72e69a02741c91ee178bc4e771e0e28d
kennizen <prachurjyagogoi123@gmail.com>
2024-04-21 11:52:41 +0530

    added connection logic

--- src/app/application/UserType.tsx
+++ src/app/application/UserType.tsx
@@ -1,17 +1,78 @@
 "use client";
 
 import { Fragment, useState } from "react";
+import { useRtcCtx } from "./page";
+import { jsonthrow } from "jsonthrow";
 
 type Usertype = "sender" | "receiver";
+type Sdp = {
+  iceCan: RTCIceCandidate[];
+  sdp: RTCSessionDescriptionInit | null;
+  remoteSdp: RTCSessionDescriptionInit | null;
+};
 
 const UserType = () => {
   // states
   const [userType, setUserType] = useState<Usertype>("sender");
+  const [session, setSession] = useState<Sdp>({
+    iceCan: [],
+    sdp: null,
+    remoteSdp: null,
+  });
 
-  // handlers
+  // hooks
+  const { peerConnection } = useRtcCtx();
+
+  // methods
   function changeUserType(type: Usertype) {
     setUserType(type);
   }
+
+  async function generateOffer() {
+    if (peerConnection === null) return;
+
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    peerConnection.onicecandidate = handleOnIceCan;
+    setSession((prev) => ({ ...prev, sdp: offer }));
+  }
+
+  async function handleOnIceCan(event: RTCPeerConnectionIceEvent) {
+    if (event.candidate) {
+      setSession((prev) => ({ ...prev, iceCan: [...prev.iceCan, event.candidate!] }));
+    }
+  }
+
+  function handleGenerateDisplayOffer() {
+    const [sdp, err] = jsonthrow.stringify(session);
+
+    if (err) {
+      console.error("Error is stringifying");
+      return "Cannot generate sdp";
+    }
+
+    return btoa(sdp);
+  }
+
+  async function handleCopySdp() {
+    let text = "";
+
+    if (session.iceCan.length > 0 && session.sdp !== null) {
+      const [sdp, err] = jsonthrow.stringify(session);
+
+      if (err) {
+        console.error("Error is stringifying");
+        return "Cannot generate sdp";
+      }
+
+      text = sdp;
+    }
+
+    await navigator.clipboard.writeText(text ? btoa(text) : "");
+  }
+
+  console.log("session", session);
 
   return (
     <div className="flex flex-col gap-8">
@@ -38,21 +99,41 @@ const UserType = () => {
         {userType === "sender" ? (
           <Fragment key="sender">
             <div className="flex flex-col gap-4">
-              <div className="border-2 border-slate-600 rounded-md w-[12rem] h-[8rem] p-2 overflow-hidden">
-                <div className="w-full h-full flex items-center justify-center">
-                  <p className="text-xs text-slate-400">No offer</p>
-                </div>
+              <div className="border-2 border-slate-600 rounded-md w-[13rem] h-[8rem] p-2">
+                {session.iceCan.length <= 0 && session.sdp === null ? (
+                  <div className="w-full h-full flex items-center justify-center">
+                    <p className="text-xs text-slate-400">No offer</p>
+                  </div>
+                ) : (
+                  <div className="group relative overflow-hidden h-full" onClick={handleCopySdp}>
+                    <p className="text-xs text-slate-400 break-words group-hover:blur-sm cursor-pointer transition-all select-none">
+                      {handleGenerateDisplayOffer()}
+                    </p>
+                    <i className="ri-clipboard-line absolute top-1/2 left-1/2 opacity-0 group-hover:opacity-100 transition-opacity translate-x-[-50%] translate-y-[-50%] pointer-events-none text-xl"></i>
+                  </div>
+                )}
               </div>
-              <button className="px-4 py-2 rounded-md outline outline-2 outline-slate-600 hover:bg-slate-600 transition-colors active:bg-slate-700">
-                Generate offer
-              </button>
+              <div className="flex items-center gap-2">
+                <button
+                  onClick={generateOffer}
+                  className="px-4 py-2 rounded-md outline outline-2 outline-slate-600 hover:bg-slate-600 transition-colors active:bg-slate-700 flex-1"
+                >
+                  Generate offer
+                </button>
+                <button
+                  onClick={handleCopySdp}
+                  className="px-4 py-2 rounded-md outline outline-2 outline-slate-600 hover:bg-slate-600 transition-colors active:bg-slate-700"
+                >
+                  <i className="ri-clipboard-line"></i>
+                </button>
+              </div>
             </div>
             <div className="flex flex-col gap-4">
               <textarea
                 name="data"
                 id="data"
                 placeholder="paste answer here..."
-                className="border-2 border-slate-600 rounded-md w-[12rem] h-[8rem] p-2 overflow-hidden bg-slate-900 resize-none text-xs focus-visible:outline-none"
+                className="border-2 border-slate-600 rounded-md w-[13rem] h-[8rem] p-2 overflow-hidden bg-slate-900 resize-none text-xs focus-visible:outline-none"
               />
               <button className="px-4 py-2 rounded-md outline outline-2 outline-slate-600 hover:bg-slate-600 transition-colors active:bg-slate-700">
                 Accept answer
@@ -66,14 +147,14 @@ const UserType = () => {
                 name="data"
                 id="data"
                 placeholder="paste offer here..."
-                className="border-2 border-slate-600 rounded-md w-[12rem] h-[8rem] p-2 overflow-hidden bg-slate-900 resize-none text-xs focus-visible:outline-none"
+                className="border-2 border-slate-600 rounded-md w-[13rem] h-[8rem] p-2 overflow-hidden bg-slate-900 resize-none text-xs focus-visible:outline-none"
               />
               <button className="px-4 py-2 rounded-md outline outline-2 outline-slate-600 hover:bg-slate-600 transition-colors active:bg-slate-700">
                 Accept offer
               </button>
             </div>
             <div className="flex flex-col gap-4">
-              <div className="border-2 border-slate-600 rounded-md w-[12rem] h-[8rem] p-2 overflow-hidden">
+              <div className="border-2 border-slate-600 rounded-md w-[13rem] h-[8rem] p-2 overflow-hidden">
                 <div className="w-full h-full flex items-center justify-center">
                   <p className="text-xs text-slate-400">No answer</p>
                 </div>
--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import ConnectionStatus from "./ConnectionStatus";
 import UserType from "./UserType";
 import WillNotWork from "./WillNotWork";
@@ -8,15 +8,46 @@ import FilePicker from "./FilePicker";
 import FileCard from "./FileCard";
 import ProgressBar from "./ProgressBar";
 
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
+    },
+  ],
+};
+
+type CtxValue = {
+  peerConnection: RTCPeerConnection | null;
+  dataChannel: RTCDataChannel | null;
+};
+
+const rtcCTx = createContext<null | CtxValue>(null);
+
+export function useRtcCtx() {
+  const ctx = useContext(rtcCTx);
+  if (ctx === null) throw new Error("rtc ctx must be used within rtc provider");
+  return ctx;
+}
+
 const Application = () => {
   // states
   const [isSupported, setIsSupported] = useState(false);
   const [files, setFiles] = useState<File[]>([]);
+  const [peerConnection, setPeerConnection] = useState<RTCPeerConnection | null>(null);
+  const [dataChannel, setDataChannel] = useState<RTCDataChannel | null>(null);
 
   // lifecycles
   useEffect(() => {
     // @ts-expect-error
-    if (window.showSaveFilePicker !== undefined) setIsSupported(true);
+    if (window.showSaveFilePicker !== undefined) {
+      setIsSupported(true);
+
+      const con = new RTCPeerConnection(servers);
+      const chan = con.createDataChannel("dataChan");
+
+      setPeerConnection(con);
+      setDataChannel(chan);
+    }
   }, []);
 
   console.log("files", files);
@@ -24,22 +55,24 @@ const Application = () => {
   if (!isSupported) return <WillNotWork />;
 
   return (
-    <div className="h-[calc(100vh-var(--navbar-height))] text-slate-100 flex justify-center">
-      <div className="max-w-[60rem] w-full py-8 flex justify-between flex-wrap gap-4 p-4 h-full">
-        <div className="flex flex-col gap-24">
-          <UserType />
-          <ProgressBar />
-        </div>
-        <div className="flex flex-col gap-10 items-end h-full">
-          <ConnectionStatus type="checking" />
-          <FilePicker onFiles={(files) => setFiles((prev) => [...files, ...prev])}>
-            {files.map((file, i) => (
-              <FileCard key={file.name + i} name={file.name} size={file.size} type={file.type} />
-            ))}
-          </FilePicker>
+    <rtcCTx.Provider value={{ peerConnection, dataChannel }}>
+      <div className="h-[calc(100dvh-var(--navbar-height))] text-slate-100 flex justify-center">
+        <div className="max-w-[60rem] w-full py-8 flex justify-between flex-wrap gap-4 p-4 h-full">
+          <div className="flex flex-col gap-24">
+            <UserType />
+            <ProgressBar />
+          </div>
+          <div className="flex flex-col gap-10 items-end h-full">
+            <ConnectionStatus type="checking" />
+            <FilePicker onFiles={(files) => setFiles((prev) => [...files, ...prev])}>
+              {files.map((file, i) => (
+                <FileCard key={file.name + i} name={file.name} size={file.size} type={file.type} />
+              ))}
+            </FilePicker>
+          </div>
         </div>
       </div>
-    </div>
+    </rtcCTx.Provider>
   );
 };
 
